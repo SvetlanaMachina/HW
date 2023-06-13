98983d60d62d3f9cc22ab5d640415287ed146370
Svetlana <swetl.burlakova2014@yandex.ru>
2023-06-13 22:24:16 +0300

    семинар1

---
+++ HW_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberB;
+if (numberA > numberB)
+{
+    Console.WriteLine ($"Максимальное число {numberA}");
+}
+if (numberC > numberA)
+{
+    Console.WriteLine ($"Максимальное число {numberC}");
+}
+else
+{
+    Console.WriteLine ($"Максимальное число {numberB}");
+}
