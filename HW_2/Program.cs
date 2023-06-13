d5712e3956a050015880d1af41ad930938413556
Svetlana <swetl.burlakova2014@yandex.ru>
2023-06-13 22:27:07 +0300

    семинар1

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// напишите программу, которая нв вход принимает два числа и выдает какое большее, какое - меньшее
+
 
 Console.Write ("Введите число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
