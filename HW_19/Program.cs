db78cce2d44cd2396c3d9e80639391724cf56a5c
Svetlana <swetl.burlakova2014@yandex.ru>
2023-06-20 14:13:46 +0300

    19 задача

--- HW_19/Program.cs
+++ HW_19/Program.cs
@@ -4,6 +4,22 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Write("Введите пятизначное число: ")
+// Console.Write ("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-
+int num,a,sum=0,b;
+Console.Write("Введите пятизначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+if(num>9999 && num<100000)
+{
+for(b=num;num!=0;num=num/10)
+{
+a=num % 10;
+sum=sum*10+a;
+}
+if(b==sum)
+Console.Write($"{b} является палиндромом");
+else
+Console.Write($"{b} не является палиндромом");
+} else
+Console.Write("Не пятизначное число!");
